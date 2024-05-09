8ece15d29a4cfefe2a05e9a308422c4fe3729b1b
prehe <philip.reher@web.de>
2024-05-09 12:12:42 +0200

    db implemetierung

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+# app.py
 from flask import Flask, render_template, request
+from model import db, Nutzer, Bezahlmöglichkeiten, Bezahlung, Produktkategorien, Produkte, Einkauf, Warenkorb
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+# Verbindung zur Datenbank herstellen
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///scannerMarket.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 # Define the root route
 @app.route('/')
@@ -66,8 +73,56 @@ def categoryPage(category):
     products = [{'name': 'fertig', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'12.0', 'manufacturer':'wert' },{'name': 'roggen', 'img':'../static/images/category-sweets.jpg', 'weight': '3.0', 'price':'4.0', 'manufacturer':'hello' },{'name': 'test', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' },{'name': 'brot', 'img':'../static/images/category-sweets.jpg', 'weight': '0.5', 'price':'6.0', 'manufacturer':'gmnt' }, {'name': 'cc', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' },{'name': 'test', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' }]
     return render_template('sm_category_page.html', category= category, products=products, banner= bannerImg)
 
+# from model import db, Nutzer
+# from datetime import date
+
+@app.route('/nutzer')
+def show_nutzer():
+    # beispiel_nutzer = Nutzer(vorname='Max', nachname='Mustermann', geb_datum=date(1990, 1, 1), email='max.mustermann@example.com', passwort='geheim', kundenkarte=True, admin=False, newsletter=True)
+    # db.session.add(beispiel_nutzer)
+    # db.session.commit()
+
+    # nutzer_entries = Nutzer.query.all() 
+    nutzer_entries = db.session.query(Nutzer).all()
+    # print(nutzer_entries[0].ID)
+    return render_template('nutzer.html', nutzer_entries=nutzer_entries)
+
+@app.route('/bezahlmöglichkeiten')
+def show_bezahlmöglichkeiten():
+    bezahlmoeglichkeiten_entries = db.session.query(Bezahlmöglichkeiten).all()
+    return render_template('bezahlmöglichkeiten.html', bezahlmoeglichkeiten_entries=bezahlmoeglichkeiten_entries)
+
+@app.route('/bezahlung')
+def show_bezahlung():
+    bezahlung_entries = db.session.query(Bezahlung).all()
+    return render_template('bezahlung.html', bezahlungen_entries =bezahlung_entries)
+
+@app.route('/produktkategorien')
+def show_produktkategorien():
+    produktkategorien_entries = db.session.query(Produktkategorien).all()
+    return render_template('produktkategorien.html', produktkategorien_entries=produktkategorien_entries)
+
+@app.route('/produkte')
+def show_produkte():
+    produkte_entries = db.session.query(Produkte).all()
+    return render_template('produkte.html', produkte_entries=produkte_entries)
+
+@app.route('/einkauf')
+def show_einkauf():
+    einkauf_entries = db.session.query(Einkauf).all()
+    return render_template('einkauf.html', einkauf_entries=einkauf_entries)
+
+@app.route('/warenkorb')
+def show_warenkorb():
+    warenkorb_entries = db.session.query(Warenkorb).all()
+    return render_template('warenkorb.html', warenkorb_entries=warenkorb_entries)
+
+
+
 # Run the Flask application
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
     
 # Liveserver nicht mehr möglich mit den child templates
