5a1da37a3181fe7681d8a6e3bd6233efeb591854
Celina Timmer <C.Timmer@beckhoff.com>
2024-05-09 09:49:22 +0200

    1. Entwurf Kategorie Seiten + dynamische Generierung + neue Bilder

--- app.py
+++ app.py
@@ -30,6 +30,42 @@ def scannerP():
 def prodBasketP():
     return render_template('sm_productbasket.html')
 
+@app.route("/category/<category>")
+#@app.route('/templates/sm_category_page/<category>')
+def categoryPage(category):
+    bannerImages={
+        'category-bread' : "../static/images/category-bread.jpg",
+        'category-can': "../static/images/category-can.jpg",
+        'category-coffee' : "../static/images/category-coffee.jpg",
+        'category-drinks' : "../static/images/category-drinks.jpg",
+        'category-fish' : "../static/images/category-fish.jpg",
+        'category-frozen' : "../static/images/category-frozen.jpg",
+        'category-fruit' : "../static/images/category-fruit.jpg",
+        'category-herbs' : "../static/images/category-herbs.jpg",
+        'category-meat' : "../static/images/category-meat.jpg",
+        'category-milk' : "../static/images/category-milk.jpg",
+        'category-pasta' : "../static/images/category-pasta.jpg", 
+        'category-sweets' : "../static/images/category-sweets.jpg"
+    }
+    categoryNames={
+        'category-bread' : "Backwaren",
+        'category-can': "Konserven & Konfitüre",
+        'category-coffee' : "Sonstiges",
+        'category-drinks' : "Getränke",
+        'category-fish' : "Fisch & Meeresfrüchte",
+        'category-frozen' : "Tiefkühlwaren",
+        'category-fruit' : "Obst & Gemüse",
+        'category-herbs' : "Gewürze & Saucen",
+        'category-meat' : "Fleisch",
+        'category-milk' : "Milchprodukte",
+        'category-pasta' : "Pasta, Reis & Nüsse", 
+        'category-sweets' : "Süßwaren"
+    }
+    bannerImg = bannerImages[category]
+    category = categoryNames[category]
+    products = [{'name': 'fertig', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'12.0', 'manufacturer':'wert' },{'name': 'roggen', 'img':'../static/images/category-sweets.jpg', 'weight': '3.0', 'price':'4.0', 'manufacturer':'hello' },{'name': 'test', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' },{'name': 'brot', 'img':'../static/images/category-sweets.jpg', 'weight': '0.5', 'price':'6.0', 'manufacturer':'gmnt' }, {'name': 'cc', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' },{'name': 'test', 'img':'../static/images/category-sweets.jpg', 'weight': '3.5', 'price':'2.0', 'manufacturer':'tt' }]
+    return render_template('sm_category_page.html', category= category, products=products, banner= bannerImg)
+
 # Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
